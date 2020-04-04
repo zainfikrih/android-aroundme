a94c99058330f8910173564c0d1ef79574e5c23e
zainfikrih <useryonoyou@gmail.com>
2020-04-04 17:43:56 +0700

    Create HomeViewModelTestSuite for run all ViewModel tests

---
+++ app/src/test/java/men/ngopi/zain/aroundme/viewmodel/ViewModelTestSuite.kt
@@ -0,0 +1,14 @@
+package men.ngopi.zain.aroundme.viewmodel
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+/**
+ * Run this to run all ViewModel tests
+ */
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    HomeViewModelTest::class
+)
+class ViewModelTestSuite
